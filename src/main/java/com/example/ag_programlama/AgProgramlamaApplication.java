d21af9f9256beec2df14d2e017d5005c50351bb4
aslidongez <a.dongez@hotmail.com>
2024-10-13 22:27:34 +0300

    Initial commit

---
+++ src/main/java/com/example/ag_programlama/AgProgramlamaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ag_programlama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgProgramlamaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgProgramlamaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ag_programlama/AgProgramlamaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ag_programlama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgProgramlamaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
